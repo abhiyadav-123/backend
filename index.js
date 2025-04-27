1cc659f6d39f19a364140525da2f9cd941cbece7
ABHISHEK YADAV <ay005562@gmail.com>
2025-04-27 09:40:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : "https://frontend-two-jade-59.vercel.app/",
     credentials : true
 }))
 app.use(express.json())
